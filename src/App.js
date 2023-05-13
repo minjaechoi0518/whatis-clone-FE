8f59062cd6f17cb9391ff4c5430170c2926c3da5
Hyunjung Cho <hyunjungcho@Hyunjungui-MacBookPro.local>
2023-05-13 18:37:39 +0900

    Feat: Navbar,회원가입 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import GnbLayout from "./layout/gnb.layout";
 
 function App() {
-  return <div className="App">클론코딩의 시작 </div>;
+  return <>
+    {/* <div className="App">클론코딩의 시작 </div> */}
+    <GnbLayout></GnbLayout>
+    <Outlet></Outlet>
+  </>
 }
 
 export default App;
